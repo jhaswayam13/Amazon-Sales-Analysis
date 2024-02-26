f78c1f7c99683f45b57aa17e79a4477a6ed55cf6
jhaswayam13 <157374540+jhaswayam13@users.noreply.github.com>
2024-02-26 08:54:39 -0800

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -7,7 +7,7 @@ import seaborn as sns
 def main():
 
     #Loading the data file
-    sales=pd.read_excel(r'D:\Swayam\technical_skills\internship projects\Amazon sales analysis\data\Amazon sales.xlsx')
+    sales=pd.read_excel(r'E:\Swayam\technical_skills\internship projects\Amazon sales analysis\data\Amazon sales.xlsx')
     print(sales.head()) #printing first 5 rows, to get insight on type of data
     sns.set_style("whitegrid")
     region_wise_profit=sales.groupby('Region')['Total Profit'].mean()
@@ -83,13 +83,39 @@ def main():
 
 
         pivot_table = item_type_sales.pivot_table(index='Region', columns='Item Type', values='Units Sold', fill_value=0)
+        #pivot_table.to_excel(r'E:\Swayam\technical_skills\internship projects\Amazon sales analysis\data\item_type_sales_pivotted.xlsx')
+        same_reg_prod2=sales[(sales.Region=='Asia') & (sales['Item Type']=='Cosmetics')]    # unit price/cost independent of order priority
+        same_reg_prod=sales[(sales.Region=='Europe')&(sales['Item Type']=='Cosmetics')] # This tells  us within a region unit price/cost is independent of channel
+        same_prod_type=sales[sales['Item Type']=='Baby Food']#unit cost and price is independnt of Region
+        
+        #conlcusion: Unit price and cost is only dependent on product type.
+        item_type_revenue=sales.groupby(['Region','Item Type'])['Total Revenue'].sum()
+        item_type_revenue=pd.DataFrame(item_type_revenue)
+        pivot_table2=item_type_revenue.pivot_table(index='Region',columns='Item Type',values='Total Revenue',fill_value=0)
+       #pivot_table2.to_excel(r'E:\Swayam\technical_skills\internship projects\Amazon sales analysis\data\item_type_revenue.xlsx')
+        sns.heatmap(data=pivot_table2,annot=True,fmt=".1f",linewidth=0.5)
+        pl.title("Regional Breakdown of Total Revenue by Item Type")
+        pl.show()
 
+
+        item_type_cost=sales.groupby(['Region','Item Type'])['Total Cost'].sum()
+        item_type_cost=pd.DataFrame(item_type_cost)
+        pivot_table3=item_type_cost.pivot_table(index='Region',columns='Item Type',values='Total Cost',fill_value=0)
+        sns.heatmap(data=pivot_table3,annot=True,fmt=".1f",linewidth=0.5)
+        pl.title("Regional Breakdown of Total Cost by Item Type")
+        pl.show()
+        #pivot_table3.to_excel(r'E:\Swayam\technical_skills\internship projects\Amazon sales analysis\data\item_type_cost.xlsx')
 
         
-   
+
+        
+
+        
+       
 
 
     EDA()
+
 
         
 
